7572c0c8ccbfbe79656056c04fd59b50f94fbf30
Sourav-Patteri <souravpatteri@gmail.com>
2020-02-12 11:40:49 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+// JavaScript Document
+let TODOItems = [];
+let completedItems = [];
+let btn = document.getElementById('btn');
+let item = document.getElementById('item');
+let listDiv = document.getElementById('list-container');
+let cross = document.getElementsByClassName("delete");
+let listItems = document.querySelectorAll('li');
+let completedList = document.getElementById('complete-container');
+
+btn.addEventListener("click", addtodo);
+
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener('click', checkOrUncheck);
+  listItems[i].previousElementSibling.addEventListener('change', function() {
+    listItems[i].click();
+  });
+
+}
+
+  delItem();  
+
+function addtodo(){
+  if (item.value.length > 0) {
+    let li = document.createElement('li');
+    let div = document.createElement('div');
+    let checkbox = document.createElement('input');
+    let delbtn = document.createElement('button');
+    delbtn.append("X");
+    setAttributes(checkbox, {"type": "checkbox", "class": "check"})
+    setAttributes(delbtn,{"class": "delete"});
+    div.setAttribute("class", "item-container");
+    li.append(item.value);
+    div.append(checkbox, li, delbtn);
+    listDiv.append(div);
+    item.value = "";
+    
+    TODOItems.push(div);
+
+    li.previousElementSibling.addEventListener('change', function() {
+      li.click();
+    });
+    li.addEventListener("click", checkOrUncheck);
+    //END STRIKETHROUGH
+    delItem();   
+  }
+}
+
+
+function delItem(){
+  for(i = 0; i < cross.length; i++){
+    cross[i].onclick = function(){
+        let itemDiv = this.parentElement;
+        itemDiv.style.display = 'none';
+    }
+  }
+}  
+
+function checkOrUncheck(e){
+  if (e.target.parentElement.classList.contains('completed')) {  
+  e.target.previousElementSibling.checked = false;
+  listDiv.append(e.target.parentElement);
+  setAttributes(e.target.parentElement, {'class': 'item-container'});
+} else {
+  e.target.style.display = 'none';
+  completedList.append(e.target.parentElement);
+  e.target.style.display = 'block';
+  e.target.previousElementSibling.checked = true;
+  setAttributes(e.target.parentElement, {'class': 'item-container completed'});
+  }
+}
+
+//helper function to set multiple attributes at once
+function setAttributes(element, attributes) {
+  for(var key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
